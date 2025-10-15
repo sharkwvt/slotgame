45b33626cb2633ddefe4a0f633d42d395a2f74f8
unotony <tony.chiang@wvt.com.tw>
2025-10-15 14:35:18 +0800

    微調

--- scenes/game/items_views.gd
+++ scenes/game/items_views.gd
@@ -60,7 +60,7 @@ func refresh_slot_items_view():
 		items_view_slot.add_child(item_bg_view)
 		if i < Slot.items.size():
 			var item: Item = Slot.items[i]
-			if Slot.is_can_use(item) and !game_scene.slot_views.in_spin:
+			if Slot.is_can_use(item) and game_scene.slot_views.can_spin():
 				var item_l_view = TextureRect.new()
 				item_l_view.texture = item_l
 				item_bg_view.add_child(item_l_view)
@@ -90,9 +90,7 @@ func get_item_view(item_id: int) -> TextureRect:
 	return
 
 func use_item(item: Item):
-	if !Slot.is_can_use(item):
-		return
-	if game_scene.slot_views.in_spin:
+	if !Slot.is_can_use(item) or !game_scene.slot_views.can_spin():
 		return
 	Slot.use_item(item)
 	game_scene.show_triggered_items()
